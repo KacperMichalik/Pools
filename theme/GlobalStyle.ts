4f4b4e126a07ecfeeaa1777426ca23080ecb5cda
Kacper Michalik <kacpermichalik3@gmail.com>
2022-05-28 18:13:39 +0200

    fix font

--- theme/GlobalStyle.ts
+++ theme/GlobalStyle.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
     h1, h2, h3, h4, h5, h6, p{
-        /* font-family: ${({ theme }) => theme.fontFamily}; */
+        font-family: ${({ theme }) => theme.fontFamily};
         margin:0;
     }
     body{

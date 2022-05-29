ad8ef0b3f8082c215f160b3c9ad7d3721b6486fb
Kacper Michalik <kacpermichalik3@gmail.com>
2022-05-29 14:32:25 +0200

    fix fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => (
   <Html>
     <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=optional"
         rel="stylesheet"
       />
     </Head>

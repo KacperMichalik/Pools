9816e09e39fdeb2e4a15626ce418f7caec8947ca
Kacper Michalik <kacpermichalik3@gmail.com>
2022-05-29 14:46:27 +0200

    fix fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,41 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// import { Html, Head, Main, NextScript } from "next/document";
+// /*eslint-disable*/
+// const Document = () => (
+//   <Html>
+//     <Head>
+//       <link
+//         href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+//         rel="stylesheet"
+//       />
+//     </Head>
+//     <body>
+//       <Main />
+//       <NextScript />
+//     </body>
+//   </Html>
+// );
 
-const Document = () => (
-  <Html>
-    <Head>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=optional"
-        rel="stylesheet"
-      />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+// export default Document;
 
-export default Document;
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

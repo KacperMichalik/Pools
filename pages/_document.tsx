bb1743f2f676230830b15b7d3f9ebef629cd55a3
Kacper Michalik <kacpermichalik3@gmail.com>
2022-05-30 22:29:41 +0200

    global styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => (
   <Html>
     <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
@@ -12,7 +12,15 @@ const Document = () => (
       <link
         href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
         rel="stylesheet"
-      />
+      /> */}
+
+      {/* <link
+        rel="preload"
+        href="../public/static/fonts/SpaceGrotesk-Regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      /> */}
     </Head>
     <body>
       <Main />

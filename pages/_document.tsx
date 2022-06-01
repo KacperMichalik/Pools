4228d20e2e36c1f4ece6da60bf77435a2523da5f
Kacper Michalik <kacpermichalik3@gmail.com>
2022-06-01 13:55:12 +0200

    fix document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,32 @@
-/* eslint-disable @next/next/no-css-tags */
-import { Html, Head, Main, NextScript } from "next/document";
-const Document = () => (
-  <Html>
-    <Head></Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default Document;
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
